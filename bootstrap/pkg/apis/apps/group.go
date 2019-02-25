c8c89c13c384ed8fbadbf946a59bf1315a73d7a1
Gabriel Wen <gabrielwen@google.com>
2019-02-25 10:27:11 -0800

    fix from merge

--- bootstrap/pkg/apis/apps/group.go
+++ bootstrap/pkg/apis/apps/group.go
@@ -37,7 +37,7 @@ const (
 	DefaultPlatform  = "ksonnet"
 	// TODO: find the latest tag dynamically
 	DefaultVersion  = "master"
-	DefaultGitRepo  = "https://github.com/kubeflow/kubeflow/tarball"
+	DefaultGitRepo  = "https://github.com/gabrielwen/kubeflow/tarball"
 	KfConfigFile    = "app.yaml"
 	DefaultCacheDir = ".cache"
 	GcpConfigDir    = "bootstrap/config"
